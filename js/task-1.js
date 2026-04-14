ffdb0f932e6254f86cbc04d9eb14bf78a7f176e0
liyayaroshenko335-cpu <liyayaroshenko335@gmail.com>
2026-04-14 11:01:42 +0300

    error

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,10 @@
+const categoriesList = document.querySelectorAll("#categories .item");
+console.log(`Number of categories: ${categoriesList.length}`);
 
-const categories = document.querySelectorAll("#categories .item");
-
-console.log(`Number of categories: ${categories.length}`);
-
-categories.forEach((category) => {
-  const categoryName = category.querySelector("h2").textContent;
+categoriesList.forEach((category) => {
+  const categoryTitle = category.querySelector("h2").textContent;
   const elementsCount = category.querySelectorAll("ul li").length;
-  console.log(`Category: ${categoryName}`);
+
+  console.log(`Category: ${categoryTitle}`);
   console.log(`Elements: ${elementsCount}`);
 });
--- js/task-2.js
+++ js/task-2.js
@@ -22,7 +22,7 @@ const images = [
   {
     url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
     alt: "Lighthouse Coast Sea",
-  }
+  },
 ];
 
 const galleryList = document.querySelector(".gallery");
@@ -35,5 +35,5 @@ const markup = images
     </li>`,
   )
   .join("");
-  
+
 galleryList.insertAdjacentHTML("beforeend", markup);
